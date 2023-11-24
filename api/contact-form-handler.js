13a94ca4bbc70b0657893696da719189fd663945
Raunak Anand <46580556+raunak26@users.noreply.github.com>
2023-11-24 12:24:49 -0800

    pls work I pray

--- api/contact-form-handler.js
+++ api/contact-form-handler.js
@@ -18,42 +18,42 @@ module.exports = async (req, res) => {
         const subject = '[IMPORTANT] New Contact Form Submission';
         const messageBody = `Name: ${name}\nEmail: ${email}\nMessage:\n${message}`;
 
-        // const transporter = nodemailer.createTransport({
-        //     host: 'smtp.gmail.com',
-        //     port: 587,
-        //     secure: false,
-        //     auth: {
-        //         user: 'anandraunak2000@gmail.com',
-        //         pass: 'jyez uwhs bakb sgke',
-        //     },
-        // });
+        const transporter = nodemailer.createTransport({
+            host: 'smtp.gmail.com',
+            port: 587,
+            secure: false,
+            auth: {
+                user: 'anandraunak2000@gmail.com',
+                pass: 'jyez uwhs bakb sgke',
+            },
+        });
 
-        // const mailOptions = {
-        //     from: `"${name}" <${email}>`,
-        //     to: to,
-        //     subject: subject,
-        //     text: messageBody, // Use messageBody instead of message
-        // };
+        const mailOptions = {
+            from: `"${name}" <${email}>`,
+            to: to,
+            subject: subject,
+            text: messageBody, // Use messageBody instead of message
+        };
 
-        res.status(200).send('Thank you for contacting us! We will get back to you soon.');
-        // try {
-        //     console.log('Trying to send email');
-        //     // transporter.sendMail(mailOptions)
-        //     //     .then(() => {
-        //     //         console.log('Successfully sent email');
-        //     //         res.status(200).send('Thank you for contacting us! We will get back to you soon.');
-        //     //     })
-        //     //     .catch((error) => {
-        //     //         console.log('Something broke');
-        //     //         console.error(error);
-        //     //         res.status(500).send('Internal Server Error');
-        //     //     });
-        //     res.status(200).send('Thank you for contacting us! We will get back to you soon.');
-        // } catch (error) {
-        //     console.log('Something broke');
-        //     console.error(error);
-        //     res.status(500).send('Internal Server Error');
-        // }
+        // res.status(200).send('Thank you for contacting us! We will get back to you soon.');
+        try {
+            console.log('Trying to send email');
+            transporter.sendMail(mailOptions)
+                .then(() => {
+                    console.log('Successfully sent email');
+                    res.status(200).send('Thank you for contacting us! We will get back to you soon.');
+                })
+                .catch((error) => {
+                    console.log('Something broke');
+                    console.error(error);
+                    res.status(500).send('Internal Server Error');
+                });
+            res.status(200).send('Thank you for contacting us! We will get back to you soon.');
+        } catch (error) {
+            console.log('Something broke');
+            console.error(error);
+            res.status(500).send('Internal Server Error');
+        }
     });
     } else {
         // Return an error for non-POST requests
