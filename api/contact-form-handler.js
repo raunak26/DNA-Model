80eac335a65cb518b9c7b256b9a2ec4ff9452a69
Raunak Anand <46580556+raunak26@users.noreply.github.com>
2023-11-24 12:07:29 -0800

    pls work

--- api/contact-form-handler.js
+++ api/contact-form-handler.js
@@ -1,68 +1,60 @@
-// const nodemailer = require("nodemailer");
-
-// export default async function handler(req, res) {
-//   if (req.method !== "POST") {
-//     return res.status(405).json({ error: "Method Not Allowed" });
-//   }
-
-//   const { name, email, message } = req.body;
-
-//   // Replace these values with your actual email configuration
-//   const transporter = nodemailer.createTransport({
-//     host: "smtp.gmail.com",
-//     port: 587,
-//     secure: false,
-//     auth: {
-//       user: "anandraunak2000@gmail.com",
-//       pass: "jyez uwhs bakb sgke",
-//     },
-//   });
-
-//   const mailOptions = {
-//     from: `"${name}" <${email}>`,
-//     to: "himay75@gmail.com",
-//     subject: "[IMPORTANT] New Contact Form Submission",
-//     text: message,
-//   };
-
-//   try {
-//     console.log("Trying to send email");
-//     // await transporter.sendMail(mailOptions);
-//     console.log("Successfully sent email");
-//     res.status(200).send('Thank you for contacting us! We will get back to you soon.');
-//   } catch (error) {
-//     console.log("Something broke");
-//     console.error(error);
-//     res.status(500).send('Internal Server Error');
-//   }
-// }
-
 const { parse } = require('querystring');
+const nodemailer = require('nodemailer');
 
 module.exports = async (req, res) => {
-  if (req.method === 'POST') {
-    let body = '';
+    if (req.method === 'POST') {
+        let body = '';
 
-    req.on('data', (chunk) => {
-      body += chunk.toString();
+        req.on('data', (chunk) => {
+            body += chunk.toString();
+        });
+
+        req.on('end', () => {
+            const formData = parse(body);
+            const { name, email, message } = formData;
+
+        // Create the email content (similar to your PHP code)
+        const to = 'himay75@gmail.com'; // Replace with your email address
+        const subject = '[IMPORTANT] New Contact Form Submission';
+        const messageBody = `Name: ${name}\nEmail: ${email}\nMessage:\n${message}`;
+
+        const transporter = nodemailer.createTransport({
+            host: 'smtp.gmail.com',
+            port: 587,
+            secure: false,
+            auth: {
+                user: 'anandraunak2000@gmail.com',
+                pass: 'jyez uwhs bakb sgke',
+            },
+        });
+
+        const mailOptions = {
+            from: `"${name}" <${email}>`,
+            to: to,
+            subject: subject,
+            text: messageBody, // Use messageBody instead of message
+        };
+
+        try {
+            console.log('Trying to send email');
+            transporter.sendMail(mailOptions)
+                .then(() => {
+                    console.log('Successfully sent email');
+                    res.status(200).send('Thank you for contacting us! We will get back to you soon.');
+                })
+                .catch((error) => {
+                    console.log('Something broke');
+                    console.error(error);
+                    res.status(500).send('Internal Server Error');
+                });
+        } catch (error) {
+            console.log('Something broke');
+            console.error(error);
+            res.status(500).send('Internal Server Error');
+        }
     });
-
-    req.on('end', () => {
-      const formData = parse(body);
-      const { name, email, message } = formData;
-
-      // Create the email content (similar to your PHP code)
-      const to = 'himay75@gmail.com'; // Replace with your email address
-      const subject = 'New Contact Form Submission';
-      const messageBody = `Name: ${name}\nEmail: ${email}\nMessage:\n${message}`;
-
-      // In a real-world scenario, you would send an email using a Node.js email library or an external service
-
-      // Send a response to the client
-      res.status(200).send('Thank you for contacting us! We will get back to you soon.');
-    });
-  } else {
-    // Return an error for non-POST requests
-    res.status(405).send('Method Not Allowed');
-  }
+    } else {
+        // Return an error for non-POST requests
+        res.status(405).send('Method Not Allowed');
+    }
 };
