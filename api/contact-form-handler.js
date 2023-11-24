8a4598faf91bf05a107f07c3d99b0f12a8110f0f
Raunak Anand <46580556+raunak26@users.noreply.github.com>
2023-11-24 11:21:55 -0800

    added app password

--- api/contact-form-handler.js
+++ api/contact-form-handler.js
@@ -1,48 +1,35 @@
-const { parse } = require('querystring');
-const nodemailer = require('nodemailer');
+const nodemailer = require("nodemailer");
 
-module.exports = async (req, res) => {
-  if (req.method === 'POST') {
-    let body = '';
-
-    req.on('data', (chunk) => {
-      body += chunk.toString();
-    });
-
-    req.on('end', async () => {
-      const formData = parse(body);
-      const { name, email, message } = formData;
-
-      // Configure the email transporter
-      const transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-          user: 'cornsandemails@yahoo.com', // Replace with your Gmail address
-          pass: 'cornucopiabio12345', // Replace with your Gmail password
-        },
-      });
-
-      // Email content
-      const mailOptions = {
-        from: 'cornsandemails@yahoo.com', // Replace with your Gmail address
-        to: 'himay75@gmail.com', // Replace with your email address
-        subject: 'New Contact Form Submission',
-        text: `Name: ${name}\nEmail: ${email}\nMessage:\n${message}`,
-      };
-
-      try {
-        // Send the email
-        await transporter.sendMail(mailOptions);
-
-        // Send a response to the client
-        res.status(200).send('Thank you for contacting us! We will get back to you soon.');
-      } catch (error) {
-        console.error(error);
-        res.status(500).send('Internal Server Error');
-      }
-    });
-  } else {
-    // Return an error for non-POST requests
-    res.status(405).send('Method Not Allowed');
+export default async function handler(req, res) {
+  if (req.method !== "POST") {
+    return res.status(405).json({ error: "Method Not Allowed" });
   }
-};
+
+  const { name, email, message } = req.body;
+
+  // Replace these values with your actual email configuration
+  const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    auth: {
+      user: "anandraunak2000@gmail.com",
+      pass: "uftd hrkw jdcg aypu",
+    },
+  });
+
+  const mailOptions = {
+    from: `"${name}" <${email}>`,
+    to: "anandraunak2000@gmail.com",
+    subject: "[IMPORTANT] New Contact Form Submission",
+    text: message,
+  };
+
+  try {
+    await transporter.sendMail(mailOptions);
+    res.status(200).send('Thank you for contacting us! We will get back to you soon.');
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
+}
